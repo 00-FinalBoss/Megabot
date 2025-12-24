538621de8bc94c677faf7dce5f2a914e2d306586
00-FinalBoss <166967123+00-FinalBoss@users.noreply.github.com>
2025-12-23 23:26:43 -0500

    Add files via upload

---
+++ triggers/join.js
@@ -0,0 +1,56 @@
+module.exports = [
+  {
+    name: "Joining the server",
+    type: "static",
+    allowedChannels: [
+      //Mega's Nook Channel IDs
+      "1217596119098593384",
+      "1403945676945096796",
+      "1259311635001249813",
+      //Mega-bot test server Channel IDs
+      "1451773291151757385",
+      "1451773330221699276",
+      "1451773347141783682",
+    ],
+    triggers: [
+      "how do i join",
+      "how can i join",
+      "how to join",
+      "how do i join the server",
+      "how do i join this server",
+      "how can i join the server",
+      "how to join the server",
+      "join server",
+      "joining",
+      "join this server",
+      "join the server",
+      "how join",
+      "how join server",
+      "how i join",
+      "how can i get in",
+      "how do i get in",
+      "get in server",
+      "getting in",
+      "can i join",
+      "can i join?",
+      "can i join this",
+      "can i join the server",
+      "am i able to join",
+      "is this open to join",
+      "how do i jon",
+      "how do i joiin",
+      "how to jon",
+      "joing server",
+      "joinig",
+      "server ip",
+      "what is the ip",
+      "ip address",
+      "server address",
+    ],
+    response: `
+**If you are looking to join the Palworld PVE server:**
+Please Read <#1450990654707470366>
+
+Also check out <#1336163521616019477> for the latest Palworld server updates`,
+  },
+];
---
+++ triggers/praise.js
@@ -0,0 +1,27 @@
+module.exports = [
+  {
+    name: "Praise",
+    type: "random",
+    triggers: [
+      "good bot",
+      "thanks bot",
+      "good mega-bot",
+      "thanks mega-bot",
+      "",
+    ],
+    responses: [
+      "“😊 Thanks!”",
+      "“I’m doing my best 😅”",
+      "I'm Thriving",
+      "Tell Mega that",
+      "Tell Final that",
+      "Wow, Thanks!",
+      "(Tail Wiggles)",
+      "Thank you",
+      "I’m so grateful.",
+      "I appreciate it.",
+      "Much obliged.",
+      "That’s so kind of you.",
+    ],
+  },
+];
