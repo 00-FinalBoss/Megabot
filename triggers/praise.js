bf3bc3abf4d47e0946859533ef6628454103d917
00-FinalBoss <166967123+00-FinalBoss@users.noreply.github.com>
2025-12-31 22:48:44 -0500

    Add files via upload

---
+++ triggers/praise.js
@@ -0,0 +1,37 @@
+module.exports = [
+  {
+    name: "Praise",
+    type: "random",
+    triggers: ["good bot", "thanks bot", "good mega-bot", "thanks mega-bot"],
+
+    blockedChannels: [
+      //Mega's Nook Channel IDs
+      "478386113938063371", //welcome
+      "1289633790095921212", //mod-view
+      "1406169487387918387", // chat commands
+      "478386069847539712", //anoucements
+      "1417657655924621392", //mega-links
+      "478389169291264007", //bot spam
+      "1294442455307124818", //auto-moderation
+      "1394880995995422800", //reminders
+
+      //Mega-bot test server Channel IDs
+      "1451773330221699276", //channel 1
+      "1451773347141783682", //channel 2
+    ],
+    responses: [
+      "“😊 Thanks!”",
+      "“I’m doing my best 😅”",
+      "I'm Thriving",
+      "Tell Mega that",
+      "Tell Final that",
+      "Wow, Thanks!",
+      "(Tail Wiggles)",
+      "Thank you",
+      "I’m so grateful.",
+      "I appreciate it.",
+      "Much obliged.",
+      "That’s so kind of you.",
+    ],
+  },
+];
---
+++ triggers/socials.js
@@ -0,0 +1,87 @@
+module.exports = [
+  {
+    name: "socials",
+    type: "static",
+
+    blockedChannels: [
+      //Mega's Nook Channel IDs
+      "478386113938063371", //welcome
+      "1289633790095921212", //mod-view
+      "1406169487387918387", // chat commands
+      "478386069847539712", //anoucements
+      "1417657655924621392", //mega-links
+      "478389169291264007", //bot spam
+      "1294442455307124818", //auto-moderation
+      "1394880995995422800", //reminders
+
+      //Mega-bot test server Channel IDs
+      "1451773330221699276", //channel 1
+      "1451773347141783682", //channel 2
+    ],
+    triggers: [
+      "Mega's socials",
+      "mega's socials",
+      "mega socials",
+      "Mega socials",
+      "socials",
+      "social media",
+      "social media links",
+      "links to socials",
+      "links to social media",
+      "where can i find your socials",
+      "where can i find your social media",
+      "can i have your socials",
+      "can i have your social media",
+      "your socials",
+      "your social media",
+      "social media?",
+      "socials?",
+      "social media links?",
+      "links to socials?",
+      "links to social media?",
+      "where can i find your socials?",
+      "where can i find your social media?",
+      "can i have your socials?",
+      "can i have your social media?",
+      "your socials?",
+      "your social media?",
+      "Mega's Twitter",
+      "Mega's Twitter?",
+      "Mega's Instagram",
+      "Mega's Instagram?",
+      "Mega's TikTok",
+      "Mega's TikTok?",
+      "Mega's YouTube",
+      "Mega's YouTube?",
+      "mega's Twitter",
+      "mega's Twitter?",
+      "mega's Instagram",
+      "mega's Instagram?",
+      "mega's TikTok",
+      "mega's TikTok?",
+      "mega's YouTube",
+      "mega's YouTube?",
+      "mega's Patreon",
+      "mega's Patreon?",
+      "Mega's Patreon",
+      "Mega's Patreon?",
+      "Mega's Merch",
+      "Mega's Merch?",
+      "mega's Merch",
+      "mega's Merch?",
+      "mega's Bluesky",
+      "mega's Bluesky?",
+      "Mega's Bluesky",
+      "Mega's Bluesky?",
+      "Mega's Throne",
+      "Mega's Throne?",
+      "mega's Throne",
+      "mega's Throne?",
+      "Mega's Throne",
+      "Mega's Throne?",
+    ],
+    response: `
+You can find Mega's socials here: <#1417657655924621392>
+`,
+  },
+];
